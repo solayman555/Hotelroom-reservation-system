31fb1cd3aaec2efae77eb76dd8b49bf3706040ad
Md Solayman Hossain Shawon <mdsolaymanhossain555@gmail.com>
2024-11-02 19:50:44 +0600

    add new

--- hotel project main.c
+++ hotel project main.c
@@ -3,7 +3,7 @@ int main()
 {
     int a;
     printf("\n\n\t\t\t");
-    for(int i=0;i<47;i++)
+    for(int i=0; i<47; i++)
     {
         printf("*");
         usleep(1000);
@@ -12,7 +12,7 @@ int main()
     printf("    \t\t\t*                                             *\n");
     printf("    \t\t\t*                                             *\n");
     printf("    \t\t\t*\t   ");
- for(int i=0;i<25;i++)
+    for(int i=0; i<25; i++)
     {
         printf("-");
         usleep(1000);
@@ -26,59 +26,105 @@ int main()
         printf("%c",b[i]);
         usleep(100000);
     }
-printf("\t      *\n");
+    printf("\t      *\n");
 
 
     printf("    \t\t\t*\t   ");
- for(int i=0;i<25;i++)
+    for(int i=0; i<25; i++)
     {
         printf("-");
         usleep(1000);
     }
     printf("\t      *");
-printf("\n");
+    printf("\n");
     printf("    \t\t\t*                                             *\n");
     printf("    \t\t\t*                                             *\n");
-     printf("\t\t\t");
-    for(int i=0;i<47;i++)
+    printf("\t\t\t");
+    for(int i=0; i<47; i++)
     {
         printf("*");
         usleep(1000);
     }
     printf("\n\n");
-    for(int i=1;i<115;i++){
-    printf("-");
+    for(int i=1; i<115; i++)
+    {
+        printf("-");
         usleep(1000);
     }
     printf("\n\n");
+    int c=1;
     printf("\t\t\t1.Admin\n");
     printf("\t\t\t2.Users\n");
     printf("\t\tEnter Your Choice : ");
     scanf("%d",&a);
+    ////////////////////////
     switch(a)
     {
-    case 1:{
-char pass[10];
-printf("\t\tEnter Your password : ");
+    case 1:
+    {
+        char pass[10];
+X:
+        printf("\t\tEnter Your password : ");
         scanf("%s",&pass);
         if(strcmp(pass,"admin")==0)
         {
-               for(int i=1;i<115;i++){
-    printf("-");
-        usleep(1000);
-    }
-            printf("\n\t\t\t\tWelcome Dear Admin");
-            printf("\n\t\t\t\t------------------");
-            
+            for(int i=1; i<115; i++)
+            {
+                printf("~");
+                usleep(1000);
+            }
+            printf("\n\t\t\t\tWelcome Admin");
+            printf("\n\t\t\t\t-------------\n");
+            printf("\t1.VIEW booking requests\n");
+            printf("\t2.VIEW booked guests details\n");
+            printf("\t3.ENTER new guest\n");
+            int x;
+            scanf("%d",&x);
+            switch(x)
+            {
+            case 1:
+            {
+                ///////////////
+          }
+            case 2:
+            {
+//////////////////////////////
+            }
+            case 3:
+                {
+/////////////////////////////////
+                }
+            }
+
         }
         else
         {
-          printf("\n\n\t\tWrong password,Access Denied");
+            printf("\n\t\tWrong password.Access Denied\n\t\tTry again....\n");
+            c++;
+            if(c==3)break;
+            goto X;
         }
-   break;
+break;
     }
-     //  case 2:
-        //   user
+    int c;
+    case 2:
+        printf("\nHello user and welcome to DSC RESORT\n");
+                printf("1.view available rooms\n");
+   	            printf("2.request booking of room\n");
+                scanf("%d",&c);
+                switch(c)
+                {
+                case 1:
+            {
+
+            }
+                case 2:
+
+{
+
+}
+
+                }
     }
     return 0;
 }
