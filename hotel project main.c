00234a725ecf2801a04a3d331be7402fc747f164
Md Solayman Hossain Shawon <mdsolaymanhossain555@gmail.com>
2024-11-02 11:35:49 +0600

    Add files via upload

---
+++ hotel project main.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+printf("\n\n\t\t\t***********************************************\n");
+printf("    \t\t\t*                                             *\n");
+printf("    \t\t\t*                                             *\n");
+printf("    \t\t\t*          -------------------------          *\n");
+printf("    \t\t\t*          | Welcome to DSC RESORT |          *\n");
+printf("    \t\t\t*          -------------------------         *\n");
+printf("    \t\t\t*                                             *\n");
+printf("    \t\t\t*                                             *\n");
+printf("    \t\t\t***********************************************\n");
+printf("---------------------------------------------------------------------------------------------------------------------\n");
+printf("1.Admin\n");
+printf("2.Users");
+scanf("%d",&a);
+while(a)
+{
+case 1:
+    //    ADmin
+    break;
+case 2:
+    //   user
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
