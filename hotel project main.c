a91070284a677554326a34ac40ec17782dc770f5
Md Solayman Hossain Shawon <mdsolaymanhossain555@gmail.com>
2024-11-02 11:58:22 +0600

    Update hotel project main.c

--- hotel project main.c
+++ hotel project main.c
@@ -2,41 +2,39 @@
 int main()
 {
     int a;
-printf("\n\n\t\t\t***********************************************\n");
-printf("    \t\t\t*                                             *\n");
-printf("    \t\t\t*                                             *\n");
-printf("    \t\t\t*          -------------------------          *\n");
-printf("    \t\t\t*          | Welcome to DSC RESORT |          *\n");
-printf("    \t\t\t*          -------------------------         *\n");
-printf("    \t\t\t*                                             *\n");
-printf("    \t\t\t*                                             *\n");
-printf("    \t\t\t***********************************************\n");
-printf("---------------------------------------------------------------------------------------------------------------------\n");
-printf("1.Admin\n");
-printf("2.Users");
-scanf("%d",&a);
-while(a)
-{
-case 1:
-    //    ADmin
-    break;
-case 2:
-    //   user
+    printf("\n\n\t\t\t***********************************************\n");
+    printf("    \t\t\t*                                             *\n");
+    printf("    \t\t\t*                                             *\n");
+    printf("    \t\t\t*          -------------------------          *\n");
+    printf("    \t\t\t*          | Welcome to DSC RESORT |          *\n");
+    printf("    \t\t\t*          -------------------------          *\n");
+    printf("    \t\t\t*                                             *\n");
+    printf("    \t\t\t*                                             *\n");
+    printf("    \t\t\t***********************************************\n");
+    printf("---------------------------------------------------------------------------------------------------------------------\n");
+    printf("1.Admin\n");
+    printf("2.Users\n");
+    printf("Enter Your Choice : ");
+    scanf("%d",&a);
+    switch(a)
+    {
+    case 1:{
+char pass[100];
+printf("Enter Your password : ");
+        scanf("%s",&pass);
+        if(strcmp(pass,"admin")==0)
+        {
+            printf("\t\t\tWelocome Dear Admin");
+        }
+        else
+        {
+          printf("Wrong password,Access Denied");
+        }
+   break;
+    }
+     //  case 2:
+        //   user
+    }
+    return 0;
 }
-return 0;
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
-}
